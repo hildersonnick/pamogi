5482c80429518f6f3515ec7c4d422049bf2bb642
Bora Baflı <borabafli@gmail.com>
2023-02-03 19:39:52 +0300

    sideBar highlight fixed

--- src/dashboard/UI/SideBar.js
+++ src/dashboard/UI/SideBar.js
@@ -1,7 +1,20 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 import classes from "./SideBar.module.css";
-import {IconCategory2,IconWorld,IconCheckbox,IconBulb,IconWallet} from "@tabler/icons";
+import {
+  IconCategory2,
+  IconWorld,
+  IconCheckbox,
+  IconBulb,
+  IconWallet,
+} from "@tabler/icons";
+
 const SideBar = (props) => {
+  const [currentMenu, setCurrentMenu] = useState("Project Dashboard");
+
+  const handleMenuButton = (event) => {
+    setCurrentMenu(event.target.innerText);
+  };
   return (
     <div className={classes["dash_sidebar-container"]}>
       <div className={classes["dash_sidebar-container_logo"]}>
@@ -10,24 +23,69 @@ const SideBar = (props) => {
       <div className={classes.dash_buttons_section}>
         <div className={classes["dash_buttons-container"]}>
           <Link className={classes.button_menu_div} to="/">
-            <IconCategory2 className={classes["dash_project-icon"]}/>
-            <button className={classes.left_menu_buttons }onClick={props.handleMenuButton}>Project Dashboard</button>
+            <IconCategory2 className={classes["dash_project-icon"]} />
+            <button
+              className={
+                currentMenu === "Project Dashboard"
+                  ? classes["left_menu_buttons-active"]
+                  : classes.left_menu_buttons
+              }
+              onClick={handleMenuButton}
+            >
+              Project Dashboard
+            </button>
           </Link>
-          <Link  className={classes.button_menu_div} to="/projects">
-            <IconWorld className={classes["dash_project-icon"]}/>
-            <button  className={classes.left_menu_buttons }onClick={props.handleMenuButton}>Projects</button>
+          <Link className={classes.button_menu_div} to="/projects">
+            <IconWorld className={classes["dash_project-icon"]} />
+            <button
+              className={
+                currentMenu === "Projects"
+                  ? classes["left_menu_buttons-active"]
+                  : classes.left_menu_buttons
+              }
+              onClick={handleMenuButton}
+            >
+              Projects
+            </button>
           </Link>
-          <Link  className={classes.button_menu_div} to="/tasks">
-            <IconCheckbox className={classes["dash_project-icon"]}/>
-            <button className={classes.left_menu_buttons }onClick={props.handleMenuButton}>Tasks</button>
+          <Link className={classes.button_menu_div} to="/tasks">
+            <IconCheckbox className={classes["dash_project-icon"]} />
+            <button
+              className={
+                currentMenu === "Tasks"
+                  ? classes["left_menu_buttons-active"]
+                  : classes.left_menu_buttons
+              }
+              onClick={handleMenuButton}
+            >
+              Tasks
+            </button>
           </Link>
-          <Link  className={classes.button_menu_div} to="/contributions">
-            <IconBulb className={classes["dash_project-icon"]}/>
-          <button className={classes.left_menu_buttons }onClick={props.handleMenuButton}>Contributions</button>
+          <Link className={classes.button_menu_div} to="/contributions">
+            <IconBulb className={classes["dash_project-icon"]} />
+            <button
+              className={
+                currentMenu === "Contributions"
+                  ? classes["left_menu_buttons-active"]
+                  : classes.left_menu_buttons
+              }
+              onClick={handleMenuButton}
+            >
+              Contributions
+            </button>
           </Link>
-          <Link  className={classes.button_menu_div} to="/investments">
-            <IconWallet className={classes["dash_project-icon"]}/>
-            <button className={classes.left_menu_buttons }onClick={props.handleMenuButton}>Investments</button>
+          <Link className={classes.button_menu_div} to="/investments">
+            <IconWallet className={classes["dash_project-icon"]} />
+            <button
+              className={
+                currentMenu === "Investments"
+                  ? classes["left_menu_buttons-active"]
+                  : classes.left_menu_buttons
+              }
+              onClick={handleMenuButton}
+            >
+              Investments
+            </button>
           </Link>
         </div>
       </div>
