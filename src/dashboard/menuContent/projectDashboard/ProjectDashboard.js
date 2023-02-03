37c4e786d25dc951a2f4e8a577f75edeedca92dc
Bora Baflı <borabafli@gmail.com>
2023-02-03 20:55:58 +0300

    middle header menu is added

--- src/dashboard/menuContent/projectDashboard/ProjectDashboard.js
+++ src/dashboard/menuContent/projectDashboard/ProjectDashboard.js
@@ -1,42 +1,22 @@
+import HeaderBar from "../../UI/HeaderBar";
+import { useState } from "react";
 const ProjectDashboard = (props) => {
-    //   const [projects, setProjects] = useState([]);
-    //   const [loading, setLoading] = useState(true);
-  
-    //   useEffect(() => {
-    //     const fetchData = async () => {
-    //       const result = await axios(
-    //         'https://jsonplaceholder.typicode.com/users',
-    //       );
-    //       setProjects(result.data);
-    //       setLoading(false);
-    //     };
-    //     fetchData();
-    //   }, []);
-  
-    //   return (
-    //     <div>
-    //       <h1>Project Dashboard</h1>
-    //       {loading ? (
-    //         <p>Loading...</p>
-    //       ) : (
-    //         <ul>
-    //           {projects.map(project => (
-    //             <li key={project.id}>
-    //               <Link to={`/projects/${project.id}`}>{project.name}</Link>
-    //             </li>
-    //           ))}
-    //         </ul>
-    //       )}
-    //     </div>
-    //   );
-  
-    return (
+
+  const [headerData , setHeaderData] = useState("Overview");
+  const handleHeaderData = (data) => {
+    setHeaderData(data);
+  }
+  return (
+    <>
+      <HeaderBar getHeaderData={handleHeaderData}  />
       <div>
         <h1>{props.projectData}</h1>
-  
+
         <h2>Project Dashboard Menu</h2>
+        <p>{headerData}</p>
       </div>
-    );
-  };
-  
-  export default ProjectDashboard;
+    </>
+  );
+};
+
+export default ProjectDashboard;
