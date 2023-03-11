42bf2b944d88c099748c3631d9db59127004357c
Tal Halperin <tal9110@gmail.com>
2023-03-10 17:35:19 -0800

    try

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,6 @@
 // third-party
 import { configureStore } from '@reduxjs/toolkit';
 
-// project import
 import reducers from './reducers';
 
 // ==============================|| REDUX TOOLKIT - MAIN STORE ||============================== //
